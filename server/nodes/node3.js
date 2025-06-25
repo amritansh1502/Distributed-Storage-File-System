4f47671593a6545ff1d2dbf6b3c28a65f36a993c
Amritansh <amritansh1502@gmail.com>
2025-06-25 17:47:51 +0530

    nodes are modified for deploying it on render

--- server/nodes/node3.js
+++ server/nodes/node3.js
@@ -1,7 +1,7 @@
+const http = require('http');
 const io = require('socket.io-client');
 const serverUrl = process.env.SERVER_URL || 'http://localhost:5000';
 const socket = io(serverUrl);
-
 
 const nodeId = 'node3';
 
@@ -9,3 +9,14 @@ setInterval(() => {
   socket.emit('nodePing', { nodeId });
   console.log(`[${nodeId}] Health ping sent`);
 }, 5000);
+
+// Simple HTTP server to keep the service alive on Render
+const PORT = process.env.PORT || 3003;
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(`${nodeId} is running\n`);
+});
+
+server.listen(PORT, () => {
+  console.log(`${nodeId} HTTP server listening on port ${PORT}`);
+});
